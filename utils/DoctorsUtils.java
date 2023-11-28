a02b0c56bdc638662201426756675ecdf3df75f6
Melika Norouzbeygi <melikanorouzbeygi75@gmail.com>
2023-11-28 16:56:38 -0700

    feat: add a function to return a hard coded list of doctors

---
+++ utils/DoctorsUtils.java
@@ -0,0 +1,46 @@
+package utils;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.Hashtable;
+
+import entities.Calendar;
+import entities.Doctor;
+
+public class DoctorsUtils {
+
+        public static Hashtable<String, Doctor> generateDoctors() {
+                // a sample doctor list is hard coded and returned in this func
+                Hashtable<String, Doctor> doctors = new Hashtable<String, Doctor>();
+
+                Calendar calendar1 = new Calendar(LocalDateTime.of(2022, Month.DECEMBER, 15, 10, 0),
+                                LocalDateTime.of(2022, Month.DECEMBER, 15, 10, 0), false);
+                Calendar calendar2 = new Calendar(LocalDateTime.of(2022, Month.DECEMBER, 15, 11, 0),
+                                LocalDateTime.of(2022, Month.DECEMBER, 15, 10, 0), false);
+                Calendar calendar3 = new Calendar(LocalDateTime.of(2022, Month.DECEMBER, 16, 14, 0),
+                                LocalDateTime.of(2022, Month.DECEMBER, 15, 10, 0), false);
+                Calendar calendar4 = new Calendar(LocalDateTime.of(2022, Month.DECEMBER, 16, 16, 0),
+                                LocalDateTime.of(2022, Month.DECEMBER, 15, 10, 0), false);
+                Calendar calendar5 = new Calendar(LocalDateTime.of(2022, Month.DECEMBER, 17, 18, 0),
+                                LocalDateTime.of(2022, Month.DECEMBER, 15, 10, 0), false);
+                Calendar calendar6 = new Calendar(LocalDateTime.of(2022, Month.DECEMBER, 17, 19, 0),
+                                LocalDateTime.of(2022, Month.DECEMBER, 15, 10, 0), false);
+
+                ArrayList<Calendar> timing = new ArrayList<Calendar>();
+
+                timing.add(calendar1);
+                timing.add(calendar2);
+                timing.add(calendar3);
+                timing.add(calendar4);
+                timing.add(calendar5);
+                timing.add(calendar6);
+
+                Doctor doctor1 = new Doctor("Siamak Honardar", "siamak.honardar@gmail.com", "1234567", "Family Physician", timing);
+
+
+                doctors.put(doctor1.getEmail(), doctor1);
+
+                return doctors;
+        }
+}
