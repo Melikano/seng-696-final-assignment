51bfa3d0efbb13c9a0ac1837c082223a3a7971cb
SainaDaneshmandjahromi <daneshmand.saina@gmail.com>
2023-11-29 15:59:54 -0700

    add signup/login messages

--- utils/Messages.java
+++ utils/Messages.java
@@ -1,6 +1,15 @@
 package utils;
 
 public class Messages {
+
+    public final static int FAILURE = 0;
+
     public final static int DOCTORS_LISTS_REQUEST = 1;
     public final static int DOCTORS_LISTS_RESPONSE = 2;
+    
+    public final static int REGISTER_REQUEST = 3;
+    public final static int REGISTER_RESPONSE = 4;
+
+    public final static int LOGIN_REQUEST = 5;
+    public final static int LOGIN_RESPONSE = 6;
 }
