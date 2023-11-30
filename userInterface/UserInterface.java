022dcc22e0c89c6a4f6ed4b0d4cb95f2d1d999c8
Melika Norouzbeygi <melikanorouzbeygi75@gmail.com>
2023-11-30 15:26:13 -0700

    fix: fix UserInterface class

--- userInterface/UserInterface.java
+++ userInterface/UserInterface.java
@@ -1,12 +1,9 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
 
 public abstract class UserInterface{
     JFrame frame;
-    UI(String frameTitle)
+    UserInterface(String frameTitle)
     {
         frame = new JFrame(frameTitle);
         frame.setLayout(new FlowLayout());
