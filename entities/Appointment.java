9d6fac680a01c024fc5db8d98f3a9e39c1c89f0e
Melika Norouzbeygi <melikanorouzbeygi75@gmail.com>
2023-11-29 20:57:29 -0700

    feat: add Appointment entity

---
+++ entities/Appointment.java
@@ -0,0 +1,60 @@
+package entities;
+
+import java.time.LocalDateTime;
+
+public class Appointment {
+    private Integer appointmentID;
+    private String patientEmail;
+    private String doctorEmail;
+    private LocalDateTime dateTime;
+    private Integer amount; // price
+
+    public Appointment(Integer appointmentID, String patientEmail, String doctorEmail, LocalDateTime dateTime,
+                       Integer amount, Boolean paid) {
+        this.appointmentID = appointmentID;
+        this.patientEmail = patientEmail;
+        this.doctorEmail = doctorEmail;
+        this.dateTime = dateTime;
+        this.amount = amount;
+    }
+
+    public Integer getAppointmentID() {
+        return appointmentID;
+    }
+
+    public void setAppointmentID(Integer appointmentID) {
+        this.appointmentID = appointmentID;
+    }
+
+    public String getPatientEmail() {
+        return patientEmail;
+    }
+
+    public void setPatientEmail(String patientEmail) {
+        this.patientEmail = patientEmail;
+    }
+
+    public String getDoctorEmail() {
+        return doctorEmail;
+    }
+
+    public void setSoctorEmail(String doctorEmail) {
+        this.doctorEmail = doctorEmail;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+}
