357eb1eb273e494b605b084f7cefe30385353453
Melika Norouzbeygi <melikanorouzbeygi75@gmail.com>
2023-11-29 20:58:53 -0700

    feat: add needed utils for creating appointment

--- utils/Messages.java
+++ utils/Messages.java
@@ -15,7 +15,11 @@ public class Messages {
 
     public final static int AVAILABILITY_REQUEST = 7;
     public final static int AVAILABILITY_RESPONSE = 8;
-    
+
+    public final static int CREATE_APPOINTMENT_REQUEST = 9;
+    public final static int CREATE_APPOINTMENT_RESPONSE = 10;
+
+
     public final static String DELIMITER = "#";
 
     public final static String MESSAGE_SUCCESS = "success";
