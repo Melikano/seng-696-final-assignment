b26c50f8441b9d6ecd581edf7455472a902bd7a2
SainaDaneshmandjahromi <daneshmand.saina@gmail.com>
2023-12-01 16:47:38 -0700

    fixing login and register

--- agents/PortalAgent.java
+++ agents/PortalAgent.java
@@ -2,7 +2,6 @@ import FIPA.DateTime;
 import jade.core.Agent;
 import jade.domain.FIPAAgentManagement.FailureException;
 import jade.lang.acl.ACLMessage;
-import utils.Messages;
 import jade.core.AID;
 import jade.core.behaviours.*;
 
@@ -10,9 +9,6 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
-import org.jcp.xml.dsig.internal.dom.Utils;
-
 import java.time.LocalDateTime;
 import jade.domain.DFService;
 import jade.domain.FIPAException;
@@ -20,11 +16,12 @@ import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
 
 
-import UserInterface;
-
 public class PortalAgent extends Agent {
     AID patientAgent;
     AID healthcareproviderAgent;
+    AID insuranceAgent;
+    AID laboratoryAgent;
+    AID pharmacyAgent;
 
     PortalUI PortalUIInstance = PortalUI.createUI(this);
 
@@ -39,16 +36,16 @@ public class PortalAgent extends Agent {
         addBehaviour(new OneShotBehaviour() {
             @Override
             public void action() {
-                PatientAgent = Messages.searchForService(myAgent, "patient");
-                healthCareProviderAgent = Messages.searchForService(myAgent, "healthcareprovider");
-                InsuranceAgent = Messages.searchForService(myAgent, "insurace");
-                LaboratoryAgent = Messages.searchForService(myAgent, "laboratory");
-                PharmacyAgent = Messages.searchForService(myAgent, "pharmacy");
+                patientAgent = ServiceSearch.searchForService(myAgent, "patient");
+                healthcareproviderAgent = ServiceSearch.searchForService(myAgent, "healthcareprovider");
+                insuranceAgent = ServiceSearch.searchForService(myAgent, "insurace");
+                laboratoryAgent = ServiceSearch.searchForService(myAgent, "laboratory");
+                pharmacyAgent = ServiceSearch.searchForService(myAgent, "pharmacy");
 
             }
         });
 
-        PortalUIInstance.startGUI();
+        PortalUIInstance.startUI();
         addBehaviour(new CyclicBehaviour() {
             public void action() {
 
@@ -159,10 +156,10 @@ public class PortalAgent extends Agent {
 
                 String payload = String.join(Messages.DELIMITER, payloadLst);
 
-                System.out.println("PORTAL: Requesting to register " + email + " to " + accessAgent.getLocalName());
+                System.out.println("PORTAL: Requesting to register " + email + " to " + patientAgent.getLocalName());
 
                 message.setContent(payload);
-                message.addReceiver(accessAgent);
+                message.addReceiver(patientAgent);
                 send(message);
             }
         });
@@ -177,10 +174,10 @@ public class PortalAgent extends Agent {
 
                 String payload = String.join(Messages.DELIMITER, payloadLst);
 
-                System.out.println("PORTAL: Requesting to login " + email + " to " + accessAgent.getLocalName());
+                System.out.println("PORTAL: Requesting to login " + email + " to " + patientAgent.getLocalName());
 
                 message.setContent(payload);
-                message.addReceiver(accessAgent);
+                message.addReceiver(patientAgent);
                 send(message);
             }
         });
--- userInterface/PatientUI.java
+++ userInterface/PatientUI.java
@@ -1,9 +1,10 @@
 import javax.swing.*;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class PatientUI extends UI implements ActionListener {
+public class PatientUI extends UserInterface implements ActionListener {
 
     JLabel descriptionTextField = new JLabel("Please login or register.");
 
